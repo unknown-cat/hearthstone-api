cb0fba40bfa941d17b8357ed90583fd2c26ca0d0
unknown-cat <twilightcat73@yahoo.com>
2022-07-19 14:25:30 +0300

    refactor: setup main app structure

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Button } from './Button/Button';
+export { default as Card } from './Card/Card';
+export { default as CardList } from './CardList/CardList';
+export { default as FormInput } from './FormInput/FormInput';
+export { default as Header } from './Header/Header';
+export { default as SearchForm } from './SearchForm/SearchForm';
+export { default as LoginForm } from './LoginForm/LoginForm';
+export { default as RegisterForm } from './RegisterForm/RegisterForm';
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home/Home';
+export { default as Login } from './Login/Login';
+export { default as Register } from './Register/Register';
+export { default as SingleCard } from './SingleCard/SingleCard';
