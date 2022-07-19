5718441a1bbf81c66393c5544ae6959d7f805cb9
unknown-cat <twilightcat73@yahoo.com>
2022-07-19 13:59:15 +0300

    feat: add login and register

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { LoginForm } from '../../components';
+
+const Login = () => {
+  return <LoginForm />;
+};
+
+export default Login;
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { RegisterForm } from '../../components';
+
+const Register = () => {
+  return <RegisterForm />;
+};
+
+export default Register;
